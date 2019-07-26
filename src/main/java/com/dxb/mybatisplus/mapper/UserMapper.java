0c4d3e8db453871ca8d5544b13c5e624e58fcd9d
戴晓波 <daixiaobo@wondersoft.cn>
2019-07-26 10:48:24 +0800

    mybatisplus+代码生成

---
+++ src/main/java/com/dxb/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.dxb.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dxb.mybatisplus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/dxb/mybatisplus/MybatisplusApplicationTests.java
@@ -0,0 +1,43 @@
+package com.dxb.mybatisplus;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.dxb.mybatisplus.entity.User;
+import com.dxb.mybatisplus.mapper.UserMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisplusApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(new QueryWrapper<User>().gt("age",20));
+        userList.forEach(System.out::println);
+        System.out.println(("----- selectAll method test ------"));
+        userList = userMapper.selectList(new QueryWrapper<User>().ge("age",20));
+        userList.forEach(System.out::println);
+        System.out.println(("----- selectAll method test ------"));
+        userList = userMapper.selectList(new QueryWrapper<User>().ge(false,"age",20));
+        userList.forEach(System.out::println);
+    }
+    @Test
+    public void testAdd(){
+        User user=new User();
+        user.setAge(11);
+        user.setEmail("xxxxx@ss.com");
+        user.setName("xxx");
+        userMapper.insert(user);
+        System.out.println(user);
+    }
+
+}
