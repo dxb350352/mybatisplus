aeca2e91dc5fcafb3abaeeff55a92bd2ccc7b2d8
戴晓波 <daixiaobo@wondersoft.cn>
2019-07-30 15:51:38 +0800

    增加动态表插件 攻击 SQL 阻断解析器

--- src/main/java/com/dxb/mybatisplus/config/MybatisPlusConfig.java
+++ src/main/java/com/dxb/mybatisplus/config/MybatisPlusConfig.java
@@ -1,10 +1,22 @@
 package com.dxb.mybatisplus.config;
 
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
+import com.baomidou.mybatisplus.extension.parsers.DynamicTableNameParser;
+import com.baomidou.mybatisplus.extension.parsers.ITableNameHandler;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @EnableTransactionManagement
 @Configuration
@@ -15,9 +27,39 @@ public class MybatisPlusConfig {
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
 
-        // 开启 count 的 join 优化,只针对 left join !!!
-        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        // 攻击 SQL 阻断解析器、加入解析链
+        sqlParserList.add(new BlockAttackSqlParser());
+
+        //动态表名 SQL 解析器
+        DynamicTableNameParser dynamicTableNameParser = new DynamicTableNameParser();
+        Map<String, ITableNameHandler> tableNameHandlerMap = new HashMap<>();
+        tableNameHandlerMap.put("user", (MetaObject metaObject, String sql, String tableName) -> {
+            if (sql.startsWith("INSERT INTO")) {
+                return "user_" + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+            }
+            return null;
+        });
+        dynamicTableNameParser.setTableNameHandlerMap(tableNameHandlerMap);
+        sqlParserList.add(dynamicTableNameParser);
+
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁插件
+     *
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+//        注解实体字段 @Version 必须要!
+//        @Version
+//        private Integer version;
+
+        return new OptimisticLockerInterceptor();
     }
 }
