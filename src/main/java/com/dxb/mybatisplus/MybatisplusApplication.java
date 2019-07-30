5b2347e13f6c5dc9c865237ca7d45acf7d331786
戴晓波 <daixiaobo@wondersoft.cn>
2019-07-30 16:12:13 +0800

    小改

--- src/main/java/com/dxb/mybatisplus/MybatisplusApplication.java
+++ src/main/java/com/dxb/mybatisplus/MybatisplusApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://mp.baomidou.com/
+ */
 @SpringBootApplication
 @MapperScan("com.dxb.mybatisplus.mapper")
 public class MybatisplusApplication {
